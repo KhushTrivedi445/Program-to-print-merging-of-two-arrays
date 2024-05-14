0820cf8303ae6de0cca62e1173528d8a3780c645
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-05-15 00:07:23 +0530

    Add files via upload

---
+++ Merging of two arrays/Merging.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int main () {
+    int n,x;
+    printf("Enter the size of first array :");
+    scanf("%d",&n);
+
+    printf("Enter the size of second array :");
+    scanf("%d",&x);
+
+    int a[n];
+    int b[x];
+
+     printf("Enter first array :");
+     for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+     }
+
+     printf("Enter second array :");
+     for(int i=0;i<x;i++){
+        scanf("%d",&b[i]);
+     }
+
+     for(int i=0;i<x;i++){
+        a[n+i]=b[i];
+     }
+
+     printf("The merged array is :");
+     for(int i=0;i<(n+x);i++){
+        printf("%d",a[i]);
+     }
+     
+     return 0;
+
+}
